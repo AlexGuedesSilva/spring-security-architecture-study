28be3babb3545ef760a9ef5ad3f9494a72fe25ac
AlexGuedesSilva <alex__guedes@hotmail.com>
2026-06-20 19:45:02 -0300

    feat(security): configure SecurityFilterChain with HTTP security rules   - Disable CSRF -Allow public access to /api/public  -Protect all other endpoints  -Enable HTTP Basic authentication

--- src/main/java/com/alexguedes/spring_security_architecture_study/security/SecurityConfig.java
+++ src/main/java/com/alexguedes/spring_security_architecture_study/security/SecurityConfig.java
@@ -1,9 +1,28 @@
 package com.alexguedes.spring_security_architecture_study.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/public").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
 }
