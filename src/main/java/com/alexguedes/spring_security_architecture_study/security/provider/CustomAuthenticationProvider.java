05f2bc79958ee4aae34ff03b7bb9651b143b8fd4
AlexGuedesSilva <alex__guedes@hotmail.com>
2026-06-20 20:05:00 -0300

    feat(security): implement custom AuthenticationProvider
    - Add custom authentication logic (username/password check)
    - Return authenticated token with ROLE_USER
    - Demonstrate Strategy Pattern with multiple providers

---
+++ src/main/java/com/alexguedes/spring_security_architecture_study/security/provider/CustomAuthenticationProvider.java
@@ -0,0 +1,45 @@
+package com.alexguedes.spring_security_architecture_study.security.provider;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CustomAuthenticationProvider
+        implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        if("alex".equals(username)
+                && "123456".equals(password)) {
+            return new UsernamePasswordAuthenticationToken(
+                    username,
+                    password,
+                    List.of(
+                            new SimpleGrantedAuthority("Role_USER")
+                    )
+            );
+        }
+
+        throw new BadCredentialsException(
+                "Invalid credentials"
+        );
+
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class
+                .isAssignableFrom(authentication);
+    }
+}
